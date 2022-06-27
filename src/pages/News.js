fe3fc49e6b795c94f75f68221229805541e6bd34
Ignacio Vazquez <ignacioew69@gmail.com>
2022-06-27 11:51:07 -0300

    news changes

--- src/pages/News.js
+++ src/pages/News.js
@@ -5,9 +5,16 @@ const News = () => {
   return (
     <div id="novedades">
     <main className="holder">
-        <div class="title">
+        <div class="title-news">
             <h1>Conoce las novedades de Coffee<span>Time</span></h1>
         </div>
+        <div className="novedades">
+            <h3>Titulo</h3>
+            <h4>Subtitulo</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit autem nemo alias dolores sunt earum minima ducimus accusamus nam rerum, temporibus eius perferendis officia. Itaque dolor vitae totam fugit officiis.</p>
+            <hr/>
+        </div>
+
         <div className="novedades">
             <h3>Titulo</h3>
             <h4>Subtitulo</h4>
